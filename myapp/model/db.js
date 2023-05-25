d3e3cc9e72e98bba0b98dccf27fc30422cccd86f
Merii-s <massil.bouzar1@gmail.com>
2023-05-25 16:10:32 +0200

    Modif compte

--- myapp/model/db.js
+++ myapp/model/db.js
@@ -2,9 +2,10 @@ var mysql = require("mysql");
 
 var pool = mysql.createPool({
     host: "tuxa.sme.utc", //ou localhost
-    user: "ai16p002",
-    password: "webAI16!",
-    database: "ai16p002"
+    user: "ai16p054", //"ai16p002",
+    password: "89PRl9zx2fnh",//'pzT76bJadOfPduAV',
+    database: "ai16p054"//"ai16p002"
 });
 
 module.exports = pool; 
+
