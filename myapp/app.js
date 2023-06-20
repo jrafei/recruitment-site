c4187e6ae0cc339a23ae7557808c02e7ff156e9f
Merii-s <massil.bouzar1@gmail.com>
2023-06-20 14:33:46 +0200

    Fix demande admin

--- myapp/app.js
+++ myapp/app.js
@@ -41,7 +41,7 @@ app.use('/', indexRouter);
 // check user
 app.all("*", function (req, res, next) {
   const nonSecurePaths = ["/inscription", "/"];
-  const adminPaths = ["/admin/userslist","/organisations/adminorga","/organisations/setadminorga","/organisations/recruteurorga","/admin/adminjoin"]; //list des urls admin
+  const adminPaths = ["/admin/userslist","/organisations/adminorga","/organisations/setadminorga","/admin/adminjoin"]; //list des urls admin
   const recruteurPaths = ["/organisations/recruteurorga","/recruteur/style.css","/recruteur/mesOffres"]; //list des urls recruteur
   if (nonSecurePaths.includes(req.path)) return next();
 
